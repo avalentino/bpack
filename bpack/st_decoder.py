f41ef3d876f3d2782803d2b8739a4cd3459ec203
Antonio Valentino <antonio.valentino@tiscali.it>
2020-11-22 22:36:57 +0100

    Struct based decoder

---
+++ bpack/st_decoder.py
@@ -0,0 +1,123 @@
+"""Bitarray based decoder for binary data structures."""
+
+import struct
+import dataclasses
+
+from typing import Optional
+
+from .utils import classdecorator
+from .descriptor import EBaseUnits
+
+
+_TYPE_SIGNED_AND_SIZE_TO_STR = {
+    # type, signed, size
+    (bool, None, 1): 'B',
+    (int, None, 1): 'b',        # default
+    (int, False, 1): 'B',
+    (int, True, 1): 'b',
+    (int, None, 2): 'h',        # default
+    (int, False, 2): 'H',
+    (int, True, 2): 'h',
+    (int, None, 4): 'i',        # default
+    (int, False, 4): 'I',
+    (int, True, 4): 'i',
+    (int, None, 8): 'q',        # default
+    (int, False, 8): 'Q',
+    (int, True, 8): 'q',
+    (float, None, 2): 'e',
+    (float, None, 4): 'f',
+    (float, None, 8): 'd',
+    (bytes, None, None): 's',
+    (str, None, None): 's',
+    (None, None, 1): 'x',       # padding
+}
+
+_DEFAULT_SIZE = {
+    bool: 1,
+    int: 4,
+    float: 4,
+    bytes: 1,
+    # str: 1,
+}
+
+
+def _to_fmt(type_, size: Optional[int] = None, order: str = '',
+            signed: Optional[bool] = None, repeat: Optional[int] = None):
+    size = _DEFAULT_SIZE.get(type_, size) if size is None else size
+
+    if size is None or size <= 0:
+        raise TypeError(f'invalid size: {size!r} for type {type_!r}')
+    if order not in ('', '>', '<', '=', '@', '!'):
+        raise TypeError(f'invalid order: {order!r}')
+    # signed = bool(signed)  # TODO: check
+
+    try:
+        if type_ in (str, bytes):
+            repeat = 1 if repeat is None else repeat
+            key = (type_, signed, None)
+            return f'{order}{size}{_TYPE_SIGNED_AND_SIZE_TO_STR[key]}' * repeat
+        else:
+            repeat = '' if repeat is None else str(repeat)
+            key = (type_, signed, size)
+            return f'{order}{repeat}{_TYPE_SIGNED_AND_SIZE_TO_STR[key]}'
+    except KeyError:
+        raise TypeError(
+            f'unable to generate format string for '
+            f'type="{type_}", size="{size}", order="{order}", '
+            f'signed="{signed}", repeat="{repeat}"')
+
+
+class Decoder:
+    def __init__(self, descriptor, *, order='>'):
+        if descriptor._BASEUNITS is not EBaseUnits.BYTES:
+            raise ValueError(
+                'struct decoder only accepts descriptors with '
+                'base units "bytes"')
+
+        if order not in ('', '>', '<', '=', '@', '!'):
+            raise TypeError(f'invalid order: {order!r}')
+
+        fmt = order + ''.join(
+            _to_fmt(field.type, field.size, order='')
+            for field in dataclasses.fields(descriptor)
+        )
+
+        self._codec = struct.Struct(fmt)
+        self._descriptor = descriptor
+        converters = {
+            str: lambda s: s.decode('ascii'),
+            bool: bool,
+            # Enums
+        }
+        # TODO: enum support
+        # converters.update(
+        #     (field.type, field.type)
+        #     for field in dataclasses.fields(self._descriptor)
+        #     if issubclass(field.type, enum.Enum))
+        self._converters = [
+            (idx, converters[field.type])
+            for idx, field in enumerate(dataclasses.fields(self._descriptor))
+            if field.type in converters
+        ]
+
+    def decode(self, data):
+        values = list(self._codec.unpack(data))
+        for idx, func in self._converters:
+            values[idx] = func(values[idx])
+        return self._descriptor(*values)
+
+
+@classdecorator
+def decoder(cls, **kwargs):
+    decoder_ = Decoder(descriptor=cls, **kwargs)
+
+    decode_func = dataclasses._create_fn(
+        name='decode',
+        args=('data',),
+        body=['return decoder.decode(data)'],
+        locals={'decoder': decoder_},
+    )
+    decode_func = staticmethod(decode_func)
+    dataclasses._set_new_attribute(cls, 'from_bytes', decode_func)
+
+    return cls
--- bpack/tests/test_decoder.py
+++ bpack/tests/test_decoder.py
@@ -4,6 +4,7 @@ import dataclasses
 from bpack.descriptor import descriptor, Field, EBaseUnits
 from bpack import ba_decoder as ba
 from bpack import bs_decoder as bs
+from bpack import st_decoder as st
 
 import pytest
 
@@ -14,33 +15,101 @@ import pytest
 
 @descriptor(baseunits=EBaseUnits.BITS)
 @dataclasses.dataclass(frozen=True)
-class BitUnitsRecord:
-    field_1: int = Field(size=3, default=0b101)
-    field_2: bool = Field(size=1, default=False)
+class BitRecord:
+    field_1: bool = Field(size=1, default=True)
+    field_2: int = Field(size=3, default=0b010)
     field_3: int = Field(size=12, default=2048)
     field_4: float = Field(size=32, default=1.)
+    field_5: bytes = Field(size=24, default=b'abc')
+    field_6: str = Field(size=24, default='ABC')
 
 
 # Big Endian
 BIT_ENCODED_DATA_BE = bytes(
     [0b10101000, 0b00000000, 0b00111111, 0b10000000, 0b00000000, 0b00000000]
-)
+) + b'abc' + 'ABC'.encode('ascii')
 
 
-@pytest.mark.parametrize('backend', [ba, bs])
+@descriptor(baseunits=EBaseUnits.BYTES)
+@dataclasses.dataclass(frozen=True)
+class ByteRecord:
+    field_01: bool = Field(size=1, default=False)
+
+    field_02: int = Field(size=1, default=1)
+    # field_03: int = Field(size=1, default=-1, signed=True)
+    # field_04: int = Field(size=1, default=+1, signed=False)
+
+    field_05: int = Field(size=2, default=2)
+    # field_06: int = Field(size=2, default=-2, signed=True)
+    # field_07: int = Field(size=2, default=+2, signed=False)
+
+    field_08: int = Field(size=4, default=4)
+    # field_09: int = Field(size=4, default=-4, signed=True)
+    # field_10: int = Field(size=4, default=+4, signed=False)
+
+    field_11: int = Field(size=8, default=8)
+    # field_12: int = Field(size=8, default=-8, signed=True)
+    # field_13: int = Field(size=8, default=+8, signed=False)
+
+    field_14: float = Field(size=2, default=10.)
+    field_15: float = Field(size=4, default=100.)
+    field_16: float = Field(size=8, default=1000.)
+
+    field_17: bytes = Field(size=3, default=b'abc')
+    field_18: str = Field(size=3, default='ABC')
+
+
+# Big Endian
+BYTE_ENCODED_DATA_BE = bytes([
+    False,                                              # bool (8 bit)
+
+    0b00000001,                                         # +1    sint8
+    # 0b11111111,                                       # -1    sint8
+    # 0b00000001,                                       # +1    uint8
+
+    0b00000000, 0b00000010,                             # +2    sint16
+    # 0b11111111, 0b11111110,                           # -2    sint16
+    # 0b00000000, 0b00000010,                           # +2    uint16
+
+    0b00000000, 0b00000000, 0b00000000, 0b00000100,     # +4    sint32
+    # 0b11111111, 0b11111111, 0b11111111, 0b11111100,   # -4    sint32
+    # 0b00000000, 0b00000000, 0b00000000, 0b00000100,   # +4    uint32
+
+    0b00000000, 0b00000000, 0b00000000, 0b00000000,
+    0b00000000, 0b00000000, 0b00000000, 0b00001000,     # +8    sint64
+    # 0b11111111, 0b11111111, 0b11111111, 0b11111111,
+    # 0b11111111, 0b11111111, 0b11111111, 0b11111000,   # -8    sint64
+    # 0b00000000, 0b00000000, 0b00000000, 0b00000000,
+    # 0b00000000, 0b00000000, 0b00000000, 0b00001000,   # +8    uint64
+
+    0b01001001, 0b00000000,                             # 10    float16
+    0b01000010, 0b11001000, 0b00000000, 0b00000000,     # 100   float32
+    0b01000000, 0b10001111, 0b01000000, 0b00000000,
+    0b00000000, 0b00000000, 0b00000000, 0b00000000,     # 1000  float64
+
+    0b01100001, 0b01100010, 0b01100011,                 # b'abc'    bytes
+    0b01000001, 0b01000010, 0b01000011,                 # 'ABC'     str
+])
+
+
 @pytest.mark.parametrize(
-    'Record, encoded_data, decoded_data',
-    [(BitUnitsRecord, BIT_ENCODED_DATA_BE, BitUnitsRecord())])
+    'backend, Record, encoded_data, decoded_data',
+    [(ba, BitRecord, BIT_ENCODED_DATA_BE, BitRecord()),
+     (bs, BitRecord, BIT_ENCODED_DATA_BE, BitRecord()),
+     (st, ByteRecord, BYTE_ENCODED_DATA_BE, ByteRecord())],
+    ids=['ba', 'bs', 'st'])
 def test_decoder(backend, Record, encoded_data, decoded_data):
     d = backend.Decoder(Record)
     record = d.decode(encoded_data)
     assert record == decoded_data
 
 
-@pytest.mark.parametrize('backend', [ba, bs])
 @pytest.mark.parametrize(
-    'Record, encoded_data, decoded_data',
-    [(BitUnitsRecord, BIT_ENCODED_DATA_BE, BitUnitsRecord())])
+    'backend, Record, encoded_data, decoded_data',
+    [(ba, BitRecord, BIT_ENCODED_DATA_BE, BitRecord()),
+     (bs, BitRecord, BIT_ENCODED_DATA_BE, BitRecord()),
+     (st, ByteRecord, BYTE_ENCODED_DATA_BE, ByteRecord())],
+    ids=['ba', 'bs', 'st'])
 def test_decoder_func(backend, Record, encoded_data, decoded_data):
     record_type = backend.decoder(Record)
     record = record_type.from_bytes(encoded_data)
@@ -48,7 +117,7 @@ def test_decoder_func(backend, Record, encoded_data, decoded_data):
 
 
 @pytest.mark.parametrize('backend', [ba, bs])
-def test_decoder_decorator(backend):
+def test_bit_decoder_decorator(backend):
     @backend.decoder
     @descriptor(baseunits=EBaseUnits.BITS)
     @dataclasses.dataclass(frozen=True)
@@ -68,14 +137,36 @@ def test_decoder_decorator(backend):
     assert record.field_4 == decoded_data.field_4
 
 
-@pytest.mark.parametrize('backend', [ba, bs])
-def test_unsupported_type(backend):
+@pytest.mark.parametrize('backend', [st], ids=['st'])
+def test_byte_decoder_decorator(backend):
+    @backend.decoder
+    @descriptor(baseunits=EBaseUnits.BYTES)
+    @dataclasses.dataclass(frozen=True)
+    class Record:
+        field_1: int = Field(size=1, default=1)
+        field_2: int = Field(size=2, default=2)
+
+    decoded_data = Record()
+    encoded_data = bytes([0b00000001, 0b00000000, 0b00000010])
+    record = Record.from_bytes(encoded_data)
+
+    assert record.field_1 == decoded_data.field_1
+    assert record.field_2 == decoded_data.field_2
+
+
+@pytest.mark.parametrize(
+    'backend, baseunits',
+    [(ba, EBaseUnits.BITS),
+     (bs, EBaseUnits.BITS),
+     (st, EBaseUnits.BYTES)],
+    ids=['ba', 'bs', 'st'])
+def test_unsupported_type(backend, baseunits):
     class CustomType:
         pass
 
     with pytest.raises(TypeError):
         @backend.decoder
-        @descriptor(baseunits=EBaseUnits.BITS)
+        @descriptor(baseunits=baseunits)
         @dataclasses.dataclass(frozen=True)
         class Record:
             field_1: CustomType = Field(size=8)
