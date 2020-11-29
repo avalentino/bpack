338c11873fc96ea4bc102fb772dcf3f40b079e3c
Antonio Valentino <antonio.valentino@tiscali.it>
2020-11-29 10:30:55 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import setuptools
+
+
+def get_version(filename, strip_extra=False):
+    import re
+    from distutils.version import LooseVersion
+    # import packaging.version
+
+    with open(filename) as fd:
+        data = fd.read()
+
+    mobj = re.search(
+        r'''^__version__\s*=\s*(?P<quote>['"])(?P<version>.*)(?P=quote)''',
+        data, re.MULTILINE)
+    version = LooseVersion(mobj.group('version'))
+
+    if strip_extra:
+        return '.'.join(map(str, version.version[:3]))
+    else:
+        return version.vstring
+
+
+cfg = dict(
+    version=get_version('bpack/__init__.py')
+)
+
+
+if __name__ == '__main__':
+    setuptools.setup(**cfg)
