8646b38c98886c8cd8f2e2fe431d428fa86683b8
Antonio Valentino <antonio.valentino@tiscali.it>
2021-01-05 20:42:10 +0100

    Update version number

--- bpack/__init__.py
+++ bpack/__init__.py
@@ -4,10 +4,11 @@ bpack provides tools to describe, in a *declarative* way, and
 encode/decode binary data.
 """
 
-__version__ = '0.6.0.dev1'
-
 from .descriptors import (                                              # noqa
     descriptor, field, fields, is_descriptor, is_field,
     baseunits, byteorder, bitorder, calcsize,
 )
 from .enums import EBaseUnits, EByteOrder, EBitOrder                    # noqa
+
+
+__version__ = '0.6.0.dev2'
