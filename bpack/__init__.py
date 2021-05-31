4d1f034bf2c1354c7493bc4ec7506d0d18ed97fb
Antonio Valentino <Antonio.Valentino@esa.int>
2021-05-31 19:43:01 +0200

    Update the version number

--- bpack/__init__.py
+++ bpack/__init__.py
@@ -12,4 +12,4 @@ from .enums import EBaseUnits, EByteOrder, EBitOrder            # noqa: F401
 from .typing import T                                           # noqa: F401
 
 
-__version__ = '0.7.2.dev0'
+__version__ = '0.8.0.dev0'
