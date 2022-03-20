1b87426e33de73b29bcb66c5560fc61aee9e8cf4
Antonio Valentino <antonio.valentino@tiscali.it>
2022-03-20 10:15:31 +0100

    Update copyright

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ sys.path.insert(0, os.path.abspath('..'))
 # -- Project information -----------------------------------------------------
 
 project = 'bpack'
-copyright = '2020-2021, Antonio Valentino'
+copyright = '2020-2022, Antonio Valentino'
 author = 'Antonio Valentino'
 
 # The full version, including alpha/beta/rc tags
