30c9c33b0fe02a78879bace4e6806323859d1d32
Antonio Valentino <antonio.valentino@tiscali.it>
2023-02-04 19:19:51 +0100

    Modernize sphinx setup

--- docs/conf.py
+++ docs/conf.py
@@ -1,14 +1,9 @@
 # Configuration file for the Sphinx documentation builder.
 #
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
+# For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 # -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
 
 import os
 import sys
@@ -16,6 +11,7 @@ sys.path.insert(0, os.path.abspath('..'))
 
 
 # -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'bpack'
 copyright = '2020-2023, Antonio Valentino'  # noqa: D100
@@ -29,10 +25,8 @@ master_doc = 'index'
 
 
 # -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.doctest',
@@ -52,32 +46,19 @@ except ImportError:
 else:
     extensions.append('sphinxcontrib.spelling')
 
-# Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
+
 # -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
 html_theme = "sphinx_rtd_theme"
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-
-# theme configuration
 html_theme_options = {
     # 'vcs_pageview_mode': 'blob',
 }
-
-# theme context
 html_context = {
     # 'github_url': 'https://github.com/avalentino/bpack/',
     'display_github': True,
@@ -88,6 +69,7 @@ html_context = {
 }
 
 html_last_updated_fmt = ''
+
 
 
 # -- Options for LaTeX output ------------------------------------------------
@@ -118,9 +100,9 @@ for module_name in ['bitarray', 'bitstruct', 'numpy']:
         autodoc_mock_imports.append(module_name)
 
 
+
 # -- Options for intersphinx extension ---------------------------------------
 
-# Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {
     'https://docs.python.org/3/': None,
     'https://numpy.org/doc/stable/': None,
@@ -132,7 +114,8 @@ extlinks = {
     'issue': ('https://github.com/avalentino/bpack/issues/%s', 'gh-%s'),
 }
 
+
+
 # -- Options for todo extension ----------------------------------------------
 
-# If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = True
