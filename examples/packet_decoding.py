78a3eda676a738ff39bd0e90e9559d92294716de
Antonio Valentino <antonio.valentino@tiscali.it>
2023-02-04 20:44:52 +0100

    Examples formatting

--- examples/packet_decoding.py
+++ examples/packet_decoding.py
@@ -17,50 +17,60 @@ class PacketDescriptor:
         return self.header_size + self.bits_per_sample * self.nsamples
 
 
-def decode_packet(data: bytes,
-                  descr: PacketDescriptor) -> Tuple[np.ndarray, np.ndarray]:
+def decode_packet(
+    data: bytes, descr: PacketDescriptor
+) -> Tuple[np.ndarray, np.ndarray]:
     if descr.header_size > 0:
-        headers = unpackbits(data, descr.header_size,
-                             samples_per_block=1,
-                             blockstride=descr.packet_size)
+        headers = unpackbits(
+            data,
+            descr.header_size,
+            samples_per_block=1,
+            blockstride=descr.packet_size,
+        )
     else:
-        headers = np.empty(shape=(), dtype='u1')
+        headers = np.empty(shape=(), dtype="u1")
 
-    samples = unpackbits(data, descr.bits_per_sample,
-                         samples_per_block=descr.nsamples,
-                         bit_offset=descr.header_size,
-                         blockstride=descr.packet_size)
+    samples = unpackbits(
+        data,
+        descr.bits_per_sample,
+        samples_per_block=descr.nsamples,
+        bit_offset=descr.header_size,
+        blockstride=descr.packet_size,
+    )
 
     return samples, headers
 
 
 def test():
     from bpack.tests.test_packbits import (
-        _make_sample_data_block as mk_sample_data
+        _make_sample_data_block as mk_sample_data,
     )
 
     descr = PacketDescriptor(header_size=13, bits_per_sample=5, nsamples=64)
-    print('descr', descr)
+    print("descr", descr)
 
-    data, values = mk_sample_data(descr.header_size, descr.bits_per_sample,
-                                  descr.nsamples, nblocks=2)
+    data, values = mk_sample_data(
+        descr.header_size, descr.bits_per_sample, descr.nsamples, nblocks=2
+    )
 
     values_per_block = 1 + descr.nsamples
     header_values = np.asarray(values[::values_per_block])
-    sample_values = np.asarray([
-        values[start:start + descr.nsamples]
-        for start in range(1, len(values), values_per_block)
-    ]).ravel()
-    print('header_values:', header_values)
-    print('sample_values:', sample_values)
+    sample_values = np.asarray(
+        [
+            values[start : start + descr.nsamples]
+            for start in range(1, len(values), values_per_block)
+        ]
+    ).ravel()
+    print("header_values:", header_values)
+    print("sample_values:", sample_values)
 
     samples, headers = decode_packet(data, descr)
-    print('headers:', headers)
-    print('samples:', samples)
+    print("headers:", headers)
+    print("samples:", samples)
 
     assert (headers == header_values).all()
     assert (samples == sample_values).all()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     test()
--- examples/s1isp.py
+++ examples/s1isp.py
@@ -8,7 +8,6 @@ import bpack
 import bpack.bs
 from bpack import T
 
-
 BITS = bpack.EBaseUnits.BITS
 BE = bpack.EByteOrder.BE
 
@@ -69,10 +68,10 @@ class EEccNumber(enum.IntEnum):
 
 class ETestMode(enum.IntEnum):
     default = 0
-    contingency_rxm_fully_operational = 4   # 100
-    contingency_rxm_fully_bypassed = 5      # 101
-    oper = 6                                # 110
-    bypass = 7                              # 111
+    contingency_rxm_fully_operational = 4  # 100
+    contingency_rxm_fully_bypassed = 5  # 101
+    oper = 6  # 110
+    bypass = 7  # 111
 
 
 class ERxChannelId(enum.IntEnum):
@@ -107,48 +106,49 @@ class ERangeDecimation(enum.IntEnum):
 @bpack.bs.decoder
 @bpack.descriptor(baseunits=BITS, byteorder=BE)
 class PacketPrimaryHeader:
-    version: T['u3'] = 0
-    packet_type: T['u1'] = 0
+    version: T["u3"] = 0
+    packet_type: T["u1"] = 0
     secondary_header_flag: bool = True
-    pid: T['u7'] = 0
-    pcat: T['u4'] = 0
-    sequence_flags: T['u2'] = 0
-    sequence_counter: T['u14'] = 0
-    packet_data_length: T['u16'] = 0
+    pid: T["u7"] = 0
+    pcat: T["u4"] = 0
+    sequence_flags: T["u2"] = 0
+    sequence_counter: T["u14"] = 0
+    packet_data_length: T["u16"] = 0
 
 
 @bpack.bs.decoder
 @bpack.descriptor(baseunits=BITS, byteorder=BE)
 class DatationService:
-    coarse_time: T['u32'] = 0
-    fine_time: T['u16'] = 0
+    coarse_time: T["u32"] = 0
+    fine_time: T["u16"] = 0
 
 
 @bpack.bs.decoder
 @bpack.descriptor(baseunits=BITS, byteorder=BE)
 class FixedAncillaryDataService:
-    sync_marker: T['u32'] = SYNK_MARKER
-    data_take_id: T['u32'] = 0
+    sync_marker: T["u32"] = SYNK_MARKER
+    data_take_id: T["u32"] = 0
     ecc_num: EEccNumber = bpack.field(size=8, default=EEccNumber.not_set)
     # n. 1 bit n/a
-    test_mode: ETestMode = bpack.field(size=3, offset=73,
-                                       default=ETestMode.default)
+    test_mode: ETestMode = bpack.field(
+        size=3, offset=73, default=ETestMode.default
+    )
     rx_channel_id: ERxChannelId = bpack.field(size=4, default=ERxChannelId.V)
-    instrument_configuration_id: T['u32'] = 0  # NOTE: the data type is TBD
+    instrument_configuration_id: T["u32"] = 0  # NOTE: the data type is TBD
 
 
 @bpack.bs.decoder
 @bpack.descriptor(baseunits=BITS, byteorder=BE)
 class SubCommunicationAncillaryDataService:
-    word_index: T['u8'] = 0
-    word_data: T['u16'] = 0
+    word_index: T["u8"] = 0
+    word_data: T["u16"] = 0
 
 
 @bpack.bs.decoder
 @bpack.descriptor(baseunits=BITS, byteorder=BE)
 class CounterService:
-    space_packet_count: T['u32'] = 0
-    pri_count: T['u32'] = 0
+    space_packet_count: T["u32"] = 0
+    pri_count: T["u32"] = 0
 
 
 @bpack.bs.decoder
@@ -156,27 +156,28 @@ class CounterService:
 class RadarConfigurationSupportService:
     error_flag: bool = False
     baq_mode: EBaqMode = bpack.field(size=5, offset=3, default=EBaqMode.bypass)
-    baq_block_len: T['u8'] = 0
+    baq_block_len: T["u8"] = 0
     # n. 8 bits padding
-    range_decimation: ERangeDecimation = bpack.field(size=8, offset=24,
-                                                     default=0)
-    rx_gain: T['u8'] = 0
-    tx_ramp_rate: T['u16'] = 0
-    tx_pulse_start_freq: T['u16'] = 0
-    tx_pulse_length: T['u24'] = 0
+    range_decimation: ERangeDecimation = bpack.field(
+        size=8, offset=24, default=0
+    )
+    rx_gain: T["u8"] = 0
+    tx_ramp_rate: T["u16"] = 0
+    tx_pulse_start_freq: T["u16"] = 0
+    tx_pulse_length: T["u24"] = 0
     # n. 3 bits pad
-    rank: T['u5'] = bpack.field(offset=99, default=0)
-    pri: T['u24'] = 0
-    swst: T['u24'] = 0
-    swl: T['u24'] = 0
-    sas_sbb_message: T['S24'] = 0       # TODO: replace with SAS sub-record
-    ses_sbb_message: T['S24'] = 0       # TODO: replace with SES sub-record
+    rank: T["u5"] = bpack.field(offset=99, default=0)
+    pri: T["u24"] = 0
+    swst: T["u24"] = 0
+    swl: T["u24"] = 0
+    sas_sbb_message: T["S24"] = 0  # TODO: replace with SAS sub-record
+    ses_sbb_message: T["S24"] = 0  # TODO: replace with SES sub-record
 
 
 @bpack.bs.decoder
 @bpack.descriptor(baseunits=BITS, byteorder=BE, size=24)
 class RadarSampleCountService:
-    number_of_quads: T['u16'] = 0
+    number_of_quads: T["u16"] = 0
     # n. 8 bits pad
 
 
@@ -200,21 +201,23 @@ def sequential_stream_decoder(filename, maxcount=None):
     log.info(f'start decoding: "{filename}"')
     t0 = datetime.datetime.now()
 
-    primary_header_size = bpack.calcsize(PacketPrimaryHeader,
-                                         bpack.EBaseUnits.BYTES)
-    secondary_header_size = bpack.calcsize(PacketSecondaryHeader,
-                                           bpack.EBaseUnits.BYTES)
+    primary_header_size = bpack.calcsize(
+        PacketPrimaryHeader, bpack.EBaseUnits.BYTES
+    )
+    secondary_header_size = bpack.calcsize(
+        PacketSecondaryHeader, bpack.EBaseUnits.BYTES
+    )
     records = []
     packet_counter = 0
-    pbar = tqdm.tqdm(unit=' packets', desc='decoded')
-    with open(filename, 'rb') as fd, pbar:
+    pbar = tqdm.tqdm(unit=" packets", desc="decoded")
+    with open(filename, "rb") as fd, pbar:
         while fd:
             # primary header
             data = fd.read(primary_header_size)
             if len(data) == 0 or (maxcount and len(records) > maxcount):
                 break
 
-            # type: PacketPrimaryHeader
+            # type - PacketPrimaryHeader
             primary_header = PacketPrimaryHeader.frombytes(data)
             # print(primary_header)
 
@@ -228,15 +231,17 @@ def sequential_stream_decoder(filename, maxcount=None):
             data_field_size = primary_header.packet_data_length + 1
             data = fd.read(data_field_size)
 
-            # type: PacketSecondaryHeader
+            # type - PacketSecondaryHeader
             secondary_header = PacketSecondaryHeader.frombytes(
-                data[:secondary_header_size])
+                data[:secondary_header_size]
+            )
             # print(secondary_header)
 
             sync = secondary_header.fixed_ancillary_data_service.sync_marker
             if sync != SYNK_MARKER:
                 raise SyncMarkerException(
-                    f'packat count: {packet_counter + 1}')
+                    f"packat count: {packet_counter + 1}"
+                )
 
             radar_cfg = secondary_header.radar_configuration_support_service
             assert radar_cfg.error_flag is False
@@ -253,13 +258,18 @@ def sequential_stream_decoder(filename, maxcount=None):
             # update the dataframe
             r = bpack.asdict(primary_header)
             r.update(bpack.asdict(secondary_header.datation_service))
-            r.update(bpack.asdict(
-                secondary_header.fixed_ancillary_data_service))
-            r.update(bpack.asdict(
-                secondary_header.subcom_ancillary_data_service))
+            r.update(
+                bpack.asdict(secondary_header.fixed_ancillary_data_service)
+            )
+            r.update(
+                bpack.asdict(secondary_header.subcom_ancillary_data_service)
+            )
             r.update(bpack.asdict(secondary_header.counters_service))
-            r.update(bpack.asdict(
-                secondary_header.radar_configuration_support_service))
+            r.update(
+                bpack.asdict(
+                    secondary_header.radar_configuration_support_service
+                )
+            )
             r.update(bpack.asdict(secondary_header.radar_sample_count_service))
             records.append(r)
 
@@ -269,29 +279,32 @@ def sequential_stream_decoder(filename, maxcount=None):
             pbar.update()
 
     elapsed = datetime.datetime.now() - t0
-    log.info(f'decoding complete (elapsed time: {elapsed})')
+    log.info(f"decoding complete (elapsed time: {elapsed})")
 
     return pd.DataFrame(records)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import os
     import sys
 
-    logging.basicConfig(level=logging.DEBUG,
-                        format='%(asctime)s %(levelname)s: %(message)s')
+    logging.basicConfig(
+        level=logging.DEBUG, format="%(asctime)s %(levelname)s: %(message)s"
+    )
 
     filename = (
-        'S1B_S3_RAW__0SDV_20210530T130904_20210530T130929_027134_033DC1_1AF2.SAFE/'
-        's1b-s3-raw-s-vv-20210530t130904-20210530t130929-027134-033dc1.dat'
+        "S1B_S3_RAW__0SDV_20210530T130904_20210530T130929_027134_033DC1_1AF2.SAFE/"
+        "s1b-s3-raw-s-vv-20210530t130904-20210530t130929-027134-033dc1.dat"
     )
     if not os.path.exists(filename):
-        sys.exit("""ERROR: sample product not available.
+        sys.exit(
+            """ERROR: sample product not available.
 
 You can download it as follows (scihub.copernicus.eu authentication needed):
 
 $ sentinelsat --name 'S1B_S3_RAW__0SDV_20200615T162409_20200615T162435_022046_029D76*' --download
-""")
+"""
+        )
 
     df = sequential_stream_decoder(filename)  # , maxcount=10)
     # print()
@@ -302,6 +315,4 @@ $ env PYTHONPATH=.. python3 s1isp.py
 2021-06-03 08:56:31,783 INFO: start decoding: "S1B_S3_RAW__0SDV_20210530T130904_20210530T130929_027134_033DC1_1AF2.SAFE/s1b-s3-raw-s-vv-20210530t130904-20210530t130929-027134-033dc1.dat"
 decoded: 48941 packets [00:19, 2506.83 packets/s]
 2021-06-03 08:56:51,320 INFO: decoding complete (elapsed time: 0:00:19.537022)
-
-
 """
